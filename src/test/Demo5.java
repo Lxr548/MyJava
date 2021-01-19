81bffc58b161664b9323520e1a19d747f748f775
lxr548 <751579190@qq.com>
2021-01-20 01:05:12 +0800

    day1

--- src/test/Demo5.java
+++ src/test/Demo5.java
@@ -5,7 +5,7 @@ public class Demo5 {
 		int g=369;
 		int l=451;
 		System.out.println("您的高考总分:"+g);
-    	System.out.println("您的高考总分:"+l);
+    	System.out.println("第一批本科录取分数线:"+l);
         if(g==(g>=l?g:l)) {
         	System.out.println("高考结果：通过一本录取分数线");
         }else {
