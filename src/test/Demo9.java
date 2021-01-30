ad4c2f6ce38e9590574518adaccd3e100bafc5d1
lxr548 <751579190@qq.com>
2021-01-30 21:47:05 +0800

    day2

--- src/test/Demo9.java
+++ src/test/Demo9.java
@@ -8,16 +8,15 @@ import java.time.LocalDateTime;
 public class Demo9 {
 
 	public static void main(String[] args) {
-		 LocalDate localDate = LocalDate.now();    
-		 LocalTime localTime = LocalTime.now(); 
-		 LocalDateTime localDateTime = LocalDateTime.now();
-		 DateTimeFormatter datefmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		 DateTimeFormatter timefmt = DateTimeFormatter.ofPattern("HH:mm:ss");
-		 DateTimeFormatter datetimefmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	     System.out.println("localDate -----"+datefmt.format(localDate));
-	     System.out.println("localTime -----"+timefmt.format(localTime));
-	     System.out.println("localDateTime -----"+datetimefmt.format(localDateTime));
-
+		LocalDate localDate = LocalDate.now();    
+		LocalTime localTime = LocalTime.now(); 
+		LocalDateTime localDateTime = LocalDateTime.now();
+		DateTimeFormatter datefmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		DateTimeFormatter timefmt = DateTimeFormatter.ofPattern("HH:mm:ss");
+		DateTimeFormatter datetimefmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		System.out.println("localDate -----"+datefmt.format(localDate));
+		System.out.println("localTime -----"+timefmt.format(localTime));
+		System.out.println("localDateTime -----"+datetimefmt.format(localDateTime));
 	}
 
 }
